2e057c8b0508ee74efd1fa8dbfa5c963c2a90a45
sbx_user1051 <sbx_user1051@169.254.125.77>
2024-11-10 18:13:26 +0000

    Sun Nov 10 18:13:26 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,25 +1,49 @@
-
+import unittest
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+prices = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+}
+
+offers = {
+    "A": {
+        3: 130
+    },
+    "B": {
+        2: 45
+    }
+}
+
+
 def checkout(skus):
-    raise NotImplementedError()
+    items = []
+    if "," in skus:
+        items = skus.split(",")
+    else:
+        items = list(skus)
+
+    items = [c for c in items]
 
 
 class TestSolution(unittest.TestCase):
-    obj = Solution()
 
     def test_1(self):
         test_cases = [
-            {"input": ([1], 9), "expected_output": -1},
+            {"input": "AAB", "expected_output": -1},
         ]
         for test_case in test_cases:
             inp = test_case["input"]
             expected = test_case["expected_output"]
             err_msg = f"Output should be {expected}, for input {inp}"
-            out = self.obj.minimumSubarrayLength(*inp)
+            out = checkout(inp)
             self.assertEqual(expected, out, err_msg)
 
 
 if __name__ == '__main__':
     unittest.main()
+
