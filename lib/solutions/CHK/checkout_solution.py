f3e18a7e4da627a65266191f5bbe3a2c673101e9
sbx_user1051 <sbx_user1051@169.254.125.77>
2024-11-10 18:07:26 +0000

    Sun Nov 10 18:07:26 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,3 +4,22 @@
 # skus = unicode string
 def checkout(skus):
     raise NotImplementedError()
+
+
+class TestSolution(unittest.TestCase):
+    obj = Solution()
+
+    def test_1(self):
+        test_cases = [
+            {"input": ([1], 9), "expected_output": -1},
+        ]
+        for test_case in test_cases:
+            inp = test_case["input"]
+            expected = test_case["expected_output"]
+            err_msg = f"Output should be {expected}, for input {inp}"
+            out = self.obj.minimumSubarrayLength(*inp)
+            self.assertEqual(expected, out, err_msg)
+
+
+if __name__ == '__main__':
+    unittest.main()
